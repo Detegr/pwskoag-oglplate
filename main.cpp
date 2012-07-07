00ec7cb45b2c668baf4cb9c81a76811794ba0545
Detegr <detegr@gmail.com>
2012-07-07 11:57:21 +0300

    Ms per frame

--- main.cpp
+++ main.cpp
@@ -3,6 +3,34 @@
 #include "singleton.h"
 #include "renderer.h"
 #include <cstdlib>
+#include <cstring>
+
+class C_MSPF
+{
+	private:
+		unsigned m_Frames;
+		double m_PrevTime;
+		double m_Time;
+		char title[64];
+	public:
+		C_MSPF() : m_Frames(0), m_PrevTime(glfwGetTime()), m_Time(-100.0f)
+		{
+			memset(title, 0, 64);
+		}
+		void M_SetTime()
+		{
+			m_Frames++;
+			double t=glfwGetTime();
+			if(1.0f + m_PrevTime <= t)
+			{
+				float mspf=(t-m_PrevTime)*1000 / m_Frames;
+				sprintf(title, "ms per frame: %f", mspf);
+				glfwSetWindowTitle(title);
+				m_PrevTime=t;
+				m_Frames=0;
+			}
+		}
+};
 
 int main()
 {
@@ -15,11 +43,19 @@ int main()
 	} else exit(1);
 	C_ModelManager* m = C_Singleton::M_ModelManager();
 	if(!m->M_Load("triangle", "test.2dmodel")) exit(1);
-	C_Entity* e = new C_Entity(m->M_Get("triangle"));
-	r->M_AddEntity(e);
+	for(int i=0; i<360; ++i)
+	{
+		C_Entity* e = new C_Entity(m->M_Get("triangle"));
+		e->M_Scale(0.285f);
+		e->M_Rotate(i*5);
+		r->M_AddEntity(e);
+	}
+	C_MSPF timer;
 	while(run)
 	{
+		timer.M_SetTime();
 		r->M_Draw();
+		glfwSleep(0.010);
 		run=!(C_Singleton::M_InputHandler()->M_Get(ESC));
 	}
 	C_Singleton::M_DestroySingletons();
